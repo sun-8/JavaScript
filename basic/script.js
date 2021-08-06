6a02a6cf628a1f127e270d2254035ae94c3888e0
sun-8 <rhdqnwjsdyd0@naver.com>
2021-08-06 17:30:35 +0900

    Update README.md and script.js

--- basic/script.js
+++ basic/script.js
@@ -11,7 +11,22 @@ console.log(myName);
 myName = "cloud";
 console.log(myName);
 //Uncaught TypeError: Assignment to constant variable.
-const yourHeight = 163;
-console.log(yourHeight);
-yourHeight = 180;
-console.log(yourHeight);
+//const yourHeight = 163;
+//console.log(yourHeight);
+//yourHeight = 180;
+//console.log(yourHeight);
+
+const amIPretty = true;
+console.log(amIPretty);
+const isyourBoyfriendHandsome = null;
+console.log(isyourBoyfriendHandsome);
+let human;
+console.log(human);
+
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+//[]를 붙이면 array가 된다.
+//[]안에 저장되는 data Type은 상관없다.
+console.log(dayOfWeek);
+console.log(dayOfWeek[4]); //금요일 찾기
+dayOfWeek.push("sun"); // 데이터 추가
+console.log(dayOfWeek); //데이터 추가 후 출력
