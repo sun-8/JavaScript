03bc52998726cff89243b23f7518160617302269
sun-8 <rhdqnwjsdyd0@naver.com>
2021-08-08 21:38:01 +0900

    Update README.md and script.js

--- basic/script.js
+++ basic/script.js
@@ -57,3 +57,63 @@ player.level = 163;
 console.log(player);
 //Uncaught TypeError: Assignment to constant variable.
 //player = true;
+
+console.log("Hello my name is Sun");
+console.log("Hello my name is Nico");
+console.log("Hello my name is June");
+console.log("Hello my name is May");
+console.log("Hello my name is April");
+//인사하는게 너무 반복적이고 길다.(복붙을 많이하고있음)
+//위의 코드에서 바뀌는 것은 이름부분일 뿐이다.
+function sayHello() {
+  //여기에 작성하는 것이 sayHello의 함수를 실행할 때마다 실행된다.
+  console.log("Hello");
+}
+//함수를 3번 실행
+sayHello();
+sayHello();
+sayHello(); //Hello가 3번 출력된다.
+//인수를 주어 함수를 통해 어떠한 값(데이터)을 보낼 수도 있다.
+//인수는 여러개 줄 수 있다.
+function hello(name, age) {
+  console.log("Hello my name is " + name + ". I'm " + age + " years old.");
+}
+hello("sun", 23);
+hello("Nico", 100);
+hello(); //함수에서는 인수를 주었는데 호출할 때 값을 넣어주지 않으면 undefined가 뜬다.
+//함수로 계산기 만들기
+function plus(a, b) {
+  console.log(a + b);
+}
+plus(); //NaN출력
+plus(1, 2);
+plus("Java", "Script");
+//xxx.xx()형식의 함수 만들기 (지금까지의 함수는 xx()형식이였다.)
+const eat = {
+  favorite: "meet",
+  fruit: function (otherFruit) {
+    console.log("apple");
+    console.log(otherFruit);
+  },
+};
+console.log(eat.favorite);
+eat.fruit(); //두개의 데이터가 출력되지만 두번째는 undifined로 출력된다.
+eat.fruit("grape");
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b); //a를 b의 갯수만큼 곱한다.
+  },
+};
+calculator.add(1, 7);
+calculator.minus(10, 10);
+calculator.divide(8, 4);
+calculator.powerOf(2, 3); //8
