9c4cbc2f9a5db5f3b847e8e5ac5a28e2813f647f
sun-8 <rhdqnwjsdyd0@naver.com>
2021-08-10 02:29:27 +0900

    Update todo.js

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -4,10 +4,11 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos)); //JSON함수로 인해 string으로 변환
+  //application에서 value로 볼수 있다(toDos array를 복사해두는 곳)
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //JSON함수로 인해 string으로 변환하여 저장
 }
 
 function deleteToDo(event) {
@@ -15,12 +16,15 @@ function deleteToDo(event) {
   //target은 클릭된 HTML element이다.(button - x)
   //parentElement는 클릭된 HTML의 부모를 가리킨다.(li)
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li"); //li 태그를 생성한다.
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❤";
 
@@ -37,8 +41,13 @@ function handleToDoSubmit(event) {
   const newTodo = toDoInput.value;
   toDoInput.value = "";
   //toDoInput.value를 비웠다고 해서 newTodo도 비워지는 것이 아니다.(둘은 별개)
-  toDos.push(newTodo); //배열에 저장
-  paintToDo(newTodo); //함수 호출하여 입력한 값을 보내줌
+  const newToDoObj = {
+    //각 할일목록들에게 id를 부여
+    text: newTodo,
+    id: Date.now(), //mm초
+  };
+  toDos.push(newToDoObj); //배열에 저장
+  paintToDo(newToDoObj); //함수 호출하여 입력한 값을 보내줌
   saveToDos(); //입력한 값저장
 }
 
@@ -48,6 +57,6 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
   const parseToDos = JSON.parse(savedToDos); //JSON함수로 배열로 만들었다.
-  console.log(parseToDos);
-  parseToDos.forEach((item) => console.log("this is thie turn of", item)); //sayHello()을 간단하게 바꾼 것 -> github histroy보기
+  toDos = parseToDos; //비어있던걸 저장한 베열을 저장함으로써 새로고침하고 값을 적어도 덮어쓰기 되지 않고 기존에 있던 것에서 추가된다.
+  parseToDos.forEach(paintToDo); //forEach함수는 paintToDo를 parseToDos배열의 요소마다 실행한다.
 }
